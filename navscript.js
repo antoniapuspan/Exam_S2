3a1f61aafb2168de034eee2b26f7c9e75505d012
AntoniaPuspan <antonia.puspan@gmail.com>
2020-12-21 11:49:35 +0100

    navbar added

---
+++ navscript.js
@@ -0,0 +1,5 @@
+function toggleBurger(){
+    document.getElementById("body-nav").classList.toggle("translate0")
+    document.getElementById("close-icon").classList.toggle("disp-none")
+    document.getElementById("burger-icon").classList.toggle("disp-none")
+}
